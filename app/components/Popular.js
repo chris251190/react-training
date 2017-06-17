3ef3c6b9c0f57a3622dbf7055cb7ff3f74de3640
Christian <christian_lehr.1@gmx.de>
2017-06-17 14:52:38 +0200

    show popular repos in a grid

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var PropTypes = require('prop-types');
+var api = require('../utils/api');
 
 function SelectLanguage(props) {
 
@@ -20,30 +21,76 @@ function SelectLanguage(props) {
         )
 }
 
+function RepoGrid(props) {
+    return(
+        <ul className='popular-list'>
+            {props.repos.map(function(repo, index) {
+                return(
+                    <li key={repo.name} className='popular-item'>
+                        <div className='popular-rank'>#{index + 1}</div>
+                        <ul className='space-list-items'>
+                            <li>
+                                <img
+                                    className='avatar'
+                                    src={repo.owner.avatar_url}
+                                    alt={'Avatar for ' + repo.owner.login}
+                                />
+                            </li>
+                            <li><a href={repo.html_url}>{repo.name}</a></li>
+                            <li>@{repo.owner.login}</li>
+                            <li>{repo.stargazers_count} stars</li>
+                        </ul>
+                    </li>
+                    )
+            })}
+        </ul>
+    )
+}
+
+RepoGrid.propTypes = {
+    repos: PropTypes.array.isRequired
+};
+
 SelectLanguage.propTypes = {
     selectedLanguage: PropTypes.string.isRequired,
     onSelect: PropTypes.func.isRequired,
-}
+};
 
 class Popular extends React.Component {
     //when using state always add a constructor
     //we need a state here to determine which link is active
     constructor (props){
-        super(props);
+        super();
         this.state = {
-            selectedLanguage: 'All'
+            selectedLanguage: 'All',
+            repos: null
         };
 
         //makes sure that updateLanguage is called with the right context
         this.updateLanguage = this.updateLanguage.bind(this);
     }
 
+    componentDidMount() {
+        this.updateLanguage(this.state.selectedLanguage);
+    }
+
     updateLanguage(lang) {
         this.setState(function () {
             return {
-                selectedLanguage: lang
+                selectedLanguage: lang,
+                repos: null
             }
             });
+
+        //AJAX
+        api.fetchPopularRepos(lang)
+            .then(function(repos){
+                this.setState(function() {
+                    return {
+                        repos: repos
+                    }
+                });
+            }.bind(this));
     }
 
     render() {
@@ -52,6 +99,8 @@ class Popular extends React.Component {
                 <SelectLanguage selectedLanguage={this.state.selectedLanguage}
                                 onSelect={this.updateLanguage}
                 />
+
+                {!this.state.repos ? <p>LOADING</p> : <RepoGrid repos={this.state.repos} />}
             </div>
         )
     }
