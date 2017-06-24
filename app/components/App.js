07c8fb37071aaf095b17f7cc36d0d347b3d8dda2
Christian <christian_lehr.1@gmx.de>
2017-06-24 12:21:17 +0200

    add playerprevie component

--- app/components/App.js
+++ app/components/App.js
@@ -2,6 +2,7 @@ var React = require('react');
 var Nav = require('./Nav');
 var Home = require('./Home');
 var Battle = require('./Battle');
+var Results = require('./Results');
 var Popular = require('./Popular');
 var ReactRouter = require('react-router-dom');
 var Router = ReactRouter.BrowserRouter;
@@ -17,6 +18,7 @@ class App extends React.Component {
                 <Switch>
                     <Route exact path="/" component={Home}/>
                     <Route exact path="/battle" component={Battle}/>
+                    <Route path='/battle/results' component={Results}/>
                     <Route path='/popular' component={Popular} />
                     <Route render={function() {
                         return <p>Not Found</p>
---
+++ app/components/Results.js
@@ -0,0 +1,11 @@
+var React = require('react');
+
+class Results extends React.Component {
+    render() {
+        return (
+            <div>Result</div>
+        )
+    }
+}
+
+module.exports = Results;
